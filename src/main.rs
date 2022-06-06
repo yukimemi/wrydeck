956f51c4b0e8bf485cb87f935c6b4832f0d9af80
yukimemi <yukimemi@gmail.com>
2022-06-07 00:03:31 +0900

    :zap: Initiall commit.

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+// Copyright 2019-2021 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+fn main() -> wry::Result<()> {
+  use wry::{
+    application::{
+      event::{Event, WindowEvent},
+      event_loop::{ControlFlow, EventLoop},
+      window::WindowBuilder,
+    },
+    webview::WebViewBuilder,
+  };
+
+  let event_loop = EventLoop::new();
+  let window = WindowBuilder::new()
+    .with_decorations(true)
+    .build(&event_loop)
+    .unwrap();
+
+  let webview = WebViewBuilder::new(window)?
+    // And the last is in html.
+    .with_url("https://tweetdeck.twitter.com")?
+    .build()?;
+
+  event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    match event {
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        ..
+      } => *control_flow = ControlFlow::Exit,
+      _ => {
+        let _ = webview.resize();
+      }
+    }
+  });
+}
